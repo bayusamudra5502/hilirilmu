02f702391c55401c8fc09e2fab7f4035fc8084a2
Bayu Samudra <bayusamudra.55.02.com@gmail.com>
2019-12-28 20:12:07 +0700

    ﷽

---
+++ index.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    // Header Trigger
+    $("#cari-trig").click(function () {
+        $("#cari-list-trig").hide();
+        $("#cari").toggleClass("d-none");
+        $("#form-cari").focus();
+    });
+
+    $("#form-cari").blur(function () {
+        $("#cari-list-trig").fadeIn("slow");
+        $("#cari").toggleClass("d-none");
+    });
+
+    // Index Main
+    $("#btnToCari").click(function () {
+        $("html,body").animate({
+            scrollTop: $("#pencarian").offset().top
+        }, 2000);
+    });
+});
