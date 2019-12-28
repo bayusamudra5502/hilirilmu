efb0c685e19d785db58efe4b236f9fde08025b35
Bayu Samudra <bayusamudra.55.02.com@gmail.com>
2019-12-28 21:30:22 +0700

    UPADTE #@

--- index.js
+++ index.js
@@ -17,4 +17,42 @@ $(document).ready(function () {
             scrollTop: $("#pencarian").offset().top
         }, 2000);
     });
+
+    $("#materiALjabar").click(function () {
+        window.location.href = "#aljabar";
+    });
+
+    $("#materiMatdis").click(function () {
+        window.location.href = "#matdis";
+    });
+
+    $("#materiGeometri").click(function () {
+        window.location.href = "#geometri";
+    });
+
+    $("#materiLogika").click(function () {
+        window.location.href = "#logika";
+    });
+
+
+    $("#materiTeoBilr").click(function () {
+        window.location.href = "#teobil";
+    });
+
+    $("#materiStatistika").click(function () {
+        window.location.href = "#statistika";
+    });
+
+    $("#materiKalkulus").click(function () {
+        window.location.href = "#kalkulus";
+    });
+
+    $("#materiAlgoritma").click(function () {
+        window.location.href = "#algoritma";
+    });
+
+    $("#materiPemrograman").click(function () {
+        window.location.href = "#programming";
+    });
+
 });
